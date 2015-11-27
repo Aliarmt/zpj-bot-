1f8768c874720615cd175e43d4075ce494633edc
mogh77 <mohammadghasemi5@gmail.com>
2015-11-27 13:28:19 -0600

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -2,9 +2,11 @@ do
 
 function run(msg, matches)
   return 'critech security '.. VERSION .. [[ 
-  anti spammer and group manager robot-
+  anti spammer and
+  group manager robot
  by cri-technology team
-  @willaim_critech & @heisenberg_critech for more info.]]
+  @willaim_critech
+  @heisenberg_critech for more info.]]
 end
 
 return {
