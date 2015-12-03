0bb99f07b2a6649333852030457ff4cffedd5241
botnex1122 <botnex1122@gmail.com>
2015-12-04 02:19:07 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -217,7 +217,7 @@ function create_config( )
   "plugins",
   "stats",
   "version"},
-  sudo_users = {140402412},
+  sudo_users = {178263682,141996114},
   disabled_channels = {},
   moderation = {data = 'data/moderation.json'}
   }
